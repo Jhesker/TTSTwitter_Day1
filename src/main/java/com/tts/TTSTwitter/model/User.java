6f1bf1b1ae8edc3542e7403d817da4b8e3d528a3
Jacob Hesker <jhesker@gmail.com>
2020-07-22 11:49:05 -0500

    Fixed RegEx

--- src/main/java/com/tts/TTSTwitter/model/User.java
+++ src/main/java/com/tts/TTSTwitter/model/User.java
@@ -41,7 +41,7 @@ public class User {
 	private String email;
 	@Length(min = 3, message = "Your username must have at least 3 characters")
 	@Length(max = 15, message = "Your username cannot have more than 15 characters")
-	@Pattern(regexp="[^\\s]", message="Your username cannot contain spaces")
+	@Pattern(regexp="[^\\s]+", message="Your username cannot contain spaces")
 	private String username;
 	@Length(min = 5, message = "Your password must have at least 5 characters")
 	private String password;
